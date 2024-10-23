8340bc652d0be1a45a44165fe19a8704d87f4103
Ksenia <Xeonica01owl@gmail.com>
2024-10-23 18:51:44 +0300

    Some modifications 2

--- js/categoriesData.js
+++ js/categoriesData.js
@@ -49,16 +49,14 @@ const categoriesData = () => {
                 listBlock.insertAdjacentHTML(`beforeend`, `
                     <div class="col-lg-4 col-md-6 col-sm-6">
                         <div class="product__item">
-                            <a href="/anime-details.html?itemId=${item.id}">
-                                <div class="product__item__pic set-bg" data-setbg="${item.image}">
-                                    <div class="ep">${item.rating} / 10</div>
-                                    <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
-                                </div>
-                                <div class="product__item__text">
-                                    ${tagsBlock.outerHTML}
-                                    <h5>${item.title}</h5>
-                                </div>
-                            </a>
+                            <div class="product__item__pic set-bg" data-setbg="${item.image}">
+                                <div class="ep">${item.rating} / 10</div>
+                                <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+                            </div>
+                            <div class="product__item__text">
+                                ${tagsBlock.outerHTML}
+                                <h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a></h5>
+                            </div>
                         </div>
                     </div>
                 `);
@@ -108,7 +106,7 @@ const categoriesData = () => {
             data.anime.forEach((item) => {
                 genres.add(item.genre);
             });
-
+            
             renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
 
             if (genreParams) {
